aa3bcc5fa8ef77c8a7911b6fec2efc8312ec79c3
Muneeb Rehman <117652966+MuneebRehman8@users.noreply.github.com>
2023-04-12 17:30:16 +0500

    update

--- src/portfolio/json.js
+++ src/portfolio/json.js
@@ -11,32 +11,89 @@ export const featuredPortfolio = [
         id: 1,
         title: "Square Box Mockup",
         img: "p1.jpg",
-        tag: "Mockup",
+        tag: "mockup",
     },
     {
         id: 2,
         title: "Product Box Package Mockup",
         img: "p2.jpg",
-        tag: "Mockup",
+        tag: "mockup",
     },{
         id: 3,
         title: "Creative Package Design",
         img: "p3.jpg",
-        tag: "Packaging",
+        tag: "packaging",
     },{
         id: 4,
         title: "Pakage Brand",
         img: "p4.jpg",
-        tag: "Packaging",
+        tag: "packaging",
     },{
         id: 5,
         title: "Isometric 3D Extrusion",
         img: "p5.jpg",
-        tag: "Typography",
+        tag: "typography",
     },{
         id: 6,
         title: "White Space Photography",
         img: "p6.jpg",
-        tag: "Photography",
+        tag: "photography",
     },
 ];
+
+export const resumeData = {
+    education: [
+        {
+            id: 1,
+            date: "March 2015 - Present",
+            title: "Masteral in Information Technology",
+            description: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.",
+            institute: "UP Open University",
+        },{
+            id: 2,
+            date: "March 2015 - Present",
+            title: "Masteral in Information Technology",
+            description: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.",
+            institute: "UP Open University",
+        },{
+            id: 3,
+            date: "March 2015 - Present",
+            title: "Masteral in Information Technology",
+            description: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.",
+            institute: "UP Open University",
+        },{
+            id: 4,
+            date: "March 2015 - Present",
+            title: "Masteral in Information Technology",
+            description: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.",
+            institute: "UP Open University",
+        },
+    ],
+    experience: [
+        {
+            id: 1,
+            date: "March 2015 - Present",
+            title: "Lead Product Designer",
+            description: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.",
+            compnay: "Facebook",
+        },{
+            id: 2,
+            date: "March 2015 - Present",
+            title: "Lead Product Designer",
+            description: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.",
+            compnay: "Google",
+        },{
+            id: 3,
+            date: "March 2015 - Present",
+            title: "Lead Product Designer",
+            description: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.",
+            compnay: "Twitter",
+        },{
+            id: 4,
+            date: "March 2015 - Present",
+            title: "Lead Product Designer",
+            description: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.",
+            compnay: "Instagram",
+        },
+    ],
+}
